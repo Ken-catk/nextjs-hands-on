979d667609ac172d2841319b5b64a8e3f2da9adb
Ken-catk <o.kengo.hackthon@gmail.com>
2021-09-01 16:11:42 +0900

    about.jsにMaterial-UI をあててみた

--- styles/theme.js
+++ styles/theme.js
@@ -2,9 +2,6 @@ import { createTheme } from '@material-ui/core/styles';
 import deepPurple from '@material-ui/core/colors/deepPurple';
 import red from '@material-ui/core/colors/red';
 
-export const palette = {
-};
-
 const theme = createTheme({
   palette: {
     primary: deepPurple,
