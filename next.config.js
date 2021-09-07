078d2cb155322ee6e6e5dbb08c8af9f4fccddecc
Ken-catk <o.kengo.hackthon@gmail.com>
2021-09-08 02:37:12 +0900

    コンフィグ周り修正

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
-const { NODE_ENV, APP_NAME, HOTPEPPER_API_KEY } = process.env;
+const { NODE_ENV, APP_NAME, VERCEL_URL, HOTPEPPER_API_KEY } = process.env;
 
 const nextConfig = {
   publicRuntimeConfig: {
     env: NODE_ENV,
     APP_NAME,
+    VERCEL_URL,
+  },
+  serverRuntimeConfig: {
     HOTPEPPER_API_KEY,
   },
 };
